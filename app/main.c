371cd9b898fec158bce341ea92ca2128240916ac
lpompa <luis.pompa@lasallenoroeste.edu.mx>
2023-03-28 16:19:06 -0700

    Plantilla base

---
+++ app/main.c
@@ -0,0 +1,36 @@
+/**
+ * @file    main.c
+ * @brief   **Template Application entry point**
+ *
+ * The main file is the entry point of the application or any user code, please provide the 
+ * proper description of this file according to your own implementation
+ *
+ * CasioCAN first real challenge
+ * 
+ * It is time for you to face a real challenge, during this phase you gonna develop an application that even
+ * though it is really simple its develop is pretty close to what you gonna face in a real job. Be ready to
+ * learn the most advanced programming techniques like reusable driver development, state machines, 
+ * schedulers, and more. Step by step you will have to increase the complexity and the features requested to 
+ * the point to reach a little more than 2000 lines of code written
+ * 
+ *
+ * @note    Only the files inside folder app will be take them into account when the 
+ *          doxygen runs by typing "make docs", index page is generated in
+ *          Build/doxigen/html/index.html
+ */
+#include "app_bsp.h"
+
+
+/**
+ * @brief   **Application entry point**
+ *
+ * Ptovide the proper description for function main according to your own
+ * implementation
+ * @retval  None
+ */
+int main( void )
+{
+   
+    return 0u;
+}
+
